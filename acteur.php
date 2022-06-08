c9a98c05aca258c6ec3a578d2668bc000f7e5f20
psychoniac <jlnko@hotmail.fr>
2022-06-08 15:15:17 +0200

    gestion de la variable get

--- acteur.php
+++ acteur.php
@@ -1,1 +1,32 @@
 <?php include('connexion_bdd.php'); ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css">
+		<title>Acteur</title>
+</head>
+
+<body>
+	<h3>Les partenaires</h3>
+	<p>
+	<?php
+	 if (isset($_GET)){
+		 var_dump($_GET); //preparer une requete pour recuperer la ligne correspondant a lid 
+	}else{
+		echo "probleme de variable";
+	}
+	  ?>
+	</p>
+	
+	
+
+
+
+</body>
+
+</html>
+
+
