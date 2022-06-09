37e2e73277f3a24b5ae1309e50d432ba48f85c9f
psychoniac <jlnko@hotmail.fr>
2022-06-09 16:53:05 +0200

    insertion fonction recuperation

--- acteur.php
+++ acteur.php
@@ -1,26 +1,39 @@
+<?php 
+
+	ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <?php include('connexion_bdd.php'); ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-<meta charset="utf-8" />
+		<meta charset="utf-8" />
 		<link rel="stylesheet" href="style.css">
 		<title>Acteur</title>
 </head>
 
 <body>
-	<h3>Les partenaires</h3>
-	<p>
-	<?php
-	 if (isset($_GET)){
-		 var_dump($_GET); //preparer une requete pour recuperer la ligne correspondant a lid 
-	}else{
-		echo "probleme de variable";
-	}
-	  ?>
-	</p>
+	<?php include('header.php'); ?>
 	
+	<h3>Les partenaires</h3>
+	<?php 
+		//verifier que $_get['id'] existe
+	
+		$requeteSQL = "SELECT * FROM acteur WHERE id_acteur = :id";
+		$requete = $db->prepare($requeteSQL);
+		$acteurselect = $requete->execute(['id' => $_GET['id']]);
+		$acteur = $requete->fetch();
+		//verifier qu'acteur ne vaut pas false
+		echo $acteur['description'];
+
+	?>
+
+	
+	<?php include('footer.php'); ?>
 	
 
 
