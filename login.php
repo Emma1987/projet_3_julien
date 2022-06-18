b83c07d4d5b227b6cdf9fe9196f9ec7072a6fe7b
psychoniac <jlnko@hotmail.fr>
2022-06-18 12:46:13 +0200

    creation formulaire de login

--- login.php
+++ login.php
@@ -10,13 +10,44 @@
 <body>
 	<?php include('header.php'); ?>
 	<h1>Se connecter :</h1>
-	<form class='login' method="post" action="home.php">
+	<form class='login' method="post" action="login.php">
 		<label for="identifiant">Mon identifiant</label>
 		<input type="text" name="identifiant" label="identifiant" id="identifiant">
 		<label for="password">Mot de passe</label>
 		<input type="password" name="password" id="password">
 		<input type="submit" name="envoyer" label="Envoyer">
 	</form>
+	<?php 
+		if  (!empty($_POST['identifiant']) && !empty($_POST['password'])) {
+			//$password = password_hash($_POST['password']);
+			$password = $_POST['password'];	
+
+			$requeteSQL = "SELECT * FROM account WHERE username = :username and password = :password";
+			$requete = $db->prepare($requeteSQL);
+			$requete->execute([
+				'username' => $_POST['identifiant'],
+				'password' => $password,
+			]);
+			$resultat = $requete->fetch();
+
+
+			if (!$resultat) {
+				echo "mot de passe ou username invalide";		
+		    } else {
+				echo "vous etes connecté!!";
+			}
+
+			//if ($resultat) {
+			//	session_start()
+			//}
+
+		}
+	?>
+					
+
+
+
+
 	<?php include('footer.php'); ?>
 
 </body>
