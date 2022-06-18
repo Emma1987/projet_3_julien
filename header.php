9041f2f41fc38ff21bf3a106108cab951fcf5fe6
psychoniac <jlnko@hotmail.fr>
2022-06-18 12:42:41 +0200

    suppression titre

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 		<header>
 
 			<img class="logo_gbaf" src="img/logo.png" alt="logo couleur de la société" width="160" height="160" title="logo de l'entreprise">
-			<h1 class="titre_principal">GBAF</h1>
+			
 		</header>
