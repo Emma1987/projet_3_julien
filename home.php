e1bd69a17b24e75b7b82292589dbcd28cb2cdf19
psychoniac <jlnko@hotmail.fr>
2022-06-18 12:45:05 +0200

    début du code du login

--- home.php
+++ home.php
@@ -8,19 +8,23 @@
 	</head>
 	
 	<body>
+		
 			<header>
 			<?php include('header.php'); ?>
 			</header>
+			
+
 			<section class="presentation">
+						<h1>GBAF</h1>
 						<h3>Présentation de GBAF</h3>
 						<p>
-				Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des pouvoirs publics.
+				Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir lactivité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des pouvoirs publics.
 				Aujourd’hui, il n’existe pas de base de données pour chercher ces informations de manière fiable et rapide ou pour donner son avis sur les partenaires et acteurs du secteur bancaire, tels que les associations ou les financeurs solidaires.
 				Pour remédier à cela, le GBAF souhaite proposer aux salariés des grands groupes français un point d’entrée unique, répertoriant un grand nombre d’informations sur les partenaires et acteurs du groupe ainsi que sur les produits et services bancaires et financiers.
 				Chaque salarié pourra ainsi poster un commentaire et donner son avis.
 						</p>
 					
-			</section>
+			</section> 
 			  
 			<section class="partenaire">
 					
@@ -33,19 +37,23 @@
 
 						foreach ($listeActeurs as $acteur) {
 							?>
-							
+							<div class="bloc_acteur">
 							<img class="logo" src="img/<?php echo $acteur['logo']; ?>">
 							
 							<p><div class="nom_acteur"><?php echo $acteur['acteur']; ?></div></p>
 							
 							<p><div class='descritpion'><?php echo $acteur['description'] ; ?> <a href="acteur.php?id=<?php echo $acteur['id_acteur']; ?>">Lire la suite</a> </div></p>
+							</div>
 							<?php
 							
 						}
 					?>
 					</div>
 			</section>
+				
 			
+			
+
 			<footer>
 
 				<?php include('footer.php'); ?>
